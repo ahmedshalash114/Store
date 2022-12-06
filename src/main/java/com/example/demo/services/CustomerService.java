745a49b731f9aeed550b701f4b3b284ddef5f51d
HassanKamel0 <hassankamel1998@gmail.com>
2022-12-06 17:54:35 +0200

    add test unit

--- src/main/java/com/example/demo/services/CustomerService.java
+++ src/main/java/com/example/demo/services/CustomerService.java
@@ -1,12 +1,10 @@
 package com.example.demo.services;
-
 import com.example.demo.DTO.Request.CustomerRequest;
 import com.example.demo.DTO.Response.CustomerResponse;
 import com.example.demo.Repository.CustomerRepository;
 import com.example.demo.Tables.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Objects;
 import java.util.Optional;
 
---
+++ src/test/java/com/example/demo/services/CustomerServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.services;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class CustomerServiceTest {
+
+    @Test
+    void getCustomer() {
+    }
+
+    @Test
+    void postCustomer() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+}
